b8a59eed5fe9d8b4b77695433b5ff4c86fccad88
Hiyab Woldegebriel <hiyab.woldegebriel@aero.org>
2024-09-18 16:55:53 -0700

    v1 landing page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./styles.scss";
 import { useNavigate } from "react-router-dom";
 import { Animate } from "react-simple-animate";
+import { Typewriter } from "react-simple-typewriter";
+import { FaLinkedin, FaGithub, FaEnvelope, FaPhone } from "react-icons/fa";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -9,30 +11,103 @@ const Home = () => {
   const handleNavigateToContactPage = () => {
     navigate("/content");
   };
+
+  const handleNavigateToPortfolioPage = () => {
+    navigate("/portfolio");
+  };
+
+  const handleNavigateToLinkedIn = () => {
+    window.location.href = "https://www.linkedin.com/in/hiyab-woldegebriel-119009178/"
+  };
+
+  const handleNavigateToGithub = () => {
+    window.location.href = "https://github.com/HiyabW"
+  };
   return (
     <section id="home" className="home">
       <div className="home__text-wrapper">
-        <h1 id="name">
-          hi, i'm heyab!
-         </h1>
-         <h1 id="description">
-          full stack developer
-        </h1>
+        <Animate
+          play
+          duration={0.5}
+          delay={0}
+          start={{
+            opacity: 0,
+            transform: "translateY(20px)",
+          }}
+          end={{
+            opacity: 1,
+            transform: "translateY(0px)",
+          }}
+        >
+          <h1 id="name">hi, i'm heyab!</h1>
+        </Animate>
+
+        <Animate
+          play
+          duration={0.5}
+          delay={1}
+          start={{
+            opacity: 0,
+          }}
+          end={{
+            opacity: 1,
+          }}
+        >
+          <h2 id="description">
+            aspiring{" "}
+            <span>
+              <Typewriter
+                cursor
+                loop={false}
+                cursorStyle="_"
+                typeSpeed={70}
+                deleteSpeed={50}
+                delaySpeed={1000}
+                words={[
+                  "frontend developer",
+                  "backend developer",
+                  "creative",
+                  "collaborator",
+                ]}
+              />
+            </span>
+          </h2>
+        </Animate>
       </div>
-      <Animate 
-      play
-      duration={0.5}
-      delay={2}
-      start={{
-        opacity: 0,
-        transform: 'translateY(20px)'
-      }}
-      end={{
-        opacity: 1,
-        transform: 'translateY(0px)'
-      }}>
-        <div className="home__contacy-me">
-          <button onClick={handleNavigateToContactPage}>Hire Me</button>
+      <Animate
+        play
+        duration={0.5}
+        delay={2}
+        start={{
+          opacity: 0,
+          transform: "translateY(20px)",
+        }}
+        end={{
+          opacity: 1,
+          transform: "translateY(0px)",
+        }}
+      >
+        <div className="home__buttons">
+          <button id="hireMe" onClick={handleNavigateToContactPage}>
+            Hire Me
+          </button>
+          <button id="viewMyWork" onClick={handleNavigateToPortfolioPage}>
+            View my Work
+          </button>
+        </div>
+        <div className="home__contactIcons">
+          <FaLinkedin
+            size={60}
+            className="home__contactIcons-icon"
+            onClick={handleNavigateToLinkedIn}
+          />
+          <FaGithub
+            size={60}
+            className="home__contactIcons-icon"
+            onClick={handleNavigateToGithub}
+          />
+          <FaEnvelope size={60} className="home__contactIcons-icon" />
+          <FaPhone size={60} className="home__contactIcons-icon" />
         </div>
       </Animate>
     </section>
